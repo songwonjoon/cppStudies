d15a7c2f4aaff13bb9e2685a1de1739f15848bd9
Jane <janeuarykim@gmail.com>
2024-12-13 14:06:27 +0900

    problem 05

---
+++ CppBasicPlus/02_GettingStarted/_05_.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double distance;
+    cout << "Enter the number of light year: ";
+    cin >> distance;
+    cout << distance << " light year = " << distance*63240 << " astronomical units.";
+}
