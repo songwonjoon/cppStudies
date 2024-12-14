a4c83d1299cbebc169043e6ebb2edad55d035e46
jeonglimLee <jllee@blitztech.kr>
2024-12-14 19:37:26 +0900

    simple change in problem1

--- CppBasicPlus/02_GettingStarted/이정림/problem01.cpp
+++ CppBasicPlus/02_GettingStarted/이정림/problem01.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 
 int main(){
-	std::cout << "name : Jlim, address : Gyeong-gi" << std::endl;
+	std::cout << "name : Jlim, address : Gyeong-gi province" << std::endl;
 	
 	return 0;
 }
