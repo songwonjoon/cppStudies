02b66e7e62e5d24b76f0c8b26adc4d7378737e32
kimsnake <khkh6380@gmail.com>
2024-12-16 18:55:28 +0900

    problem 7 upload

---
+++ CppBasicPlus/02_GettingStarted/kimgyuhwan/problem7.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {        
+    int m = 0;
+    int h = 0;
+    cout << "시간 값을 입력하시오:";
+    cin >> h;
+    cout << "분 값을 입력하시오:";
+    cin >> m;
+    cout << "시각 : "<<h<<":"<<m<<endl;    
+    return 0;
+}
